e760d087e39971cd0178cd8ea75fd754659c0512
koalaczaj <ryancrummypl@gmail.com>
2019-06-04 16:53:27 +0200

    initial commit

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { Observable } from "rxjs";
+import { of } from "rxjs";
+
+import { Log } from "../models/Log";
+import { fromEventPattern } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class LogService {
+  logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({
+    id: null,
+    text: null,
+    date: null
+  });
+  selectedLog = this.logSource.asObservable();
+
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
+  constructor() {
+    // this.logs = [
+    //   {
+    //     id: "1",
+    //     text: "Generated components",
+    //     date: new Date("12/26/2017 12:51:23")
+    //   },
+    //   {
+    //     id: "2",
+    //     text: "Added Bootstrap",
+    //     date: new Date("12/27/2017 9:51:23")
+    //   },
+    //   {
+    //     id: "3",
+    //     text: "Added logs component",
+    //     date: new Date("12/28/2017 13:51:23")
+    //   }
+    // ];
+
+    this.logs = [];
+  }
+
+  getLogs(): Observable<Log[]> {
+    if (localStorage.getItem("logs") === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem("logs"));
+    }
+
+    return of(
+      this.logs.sort((a, b) => {
+        return (b.date = a.date);
+      })
+    );
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+
+    // Add to local storage
+    localStorage.setItem("logs", JSON.stringify(this.logs));
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+    // Update to LS
+    localStorage.setItem("logs", JSON.stringify(this.logs));
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    // Delete from LS
+    localStorage.setItem("logs", JSON.stringify(this.logs));
+  }
+
+  clearState() {
+    this.stateSource.next(true);
+  }
+}
